5e8806562611071d9437cd720174919837fd5a6d
vytautasb <vytautas.buitvydas@gmail.com>
2016-10-14 16:49:04 +0300

    Connected WFM player

--- db/ip/slda31c0ea5/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/slda31c0ea5/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 8372ed50a91b006eaee7
+//   DESIGN_HASH   : 10578657f5e4100f3d71
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8372ed50a91b006eaee7 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h10578657f5e4100f3d71 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
