bafe776f400d35ce005901edffe3805299d53c29
vytautasb <vytautas.buitvydas@gmail.com>
2016-12-19 16:36:35 +0200

    Modified output file scripts to add HW version to file name

--- gen_prg_files.tcl
+++ gen_prg_files.tcl
@@ -1,8 +1,10 @@
+#Copy and Rename .sof file by hardware version 
+file copy -force -- output_files/LimeSDR-USB_lms7_trx.sof output_files/LimeSDR-USB_lms7_trx_HW_1.4.sof
 qexec "quartus_cpf -c output_files/jic_file_setup.cof"
 post_message "*******************************************************************"
-post_message "Generated programming file: LimeSDR-USB_lms7_trx.jic" -submsgs [list "Ouput file saved in /output_files directory"]
+post_message "Generated programming file: LimeSDR-USB_lms7_trx_HW_1.4.jic" -submsgs [list "Output file saved in /output_files directory"]
 post_message "*******************************************************************"
 qexec "quartus_cpf -c output_files/rbf_file_setup.cof"
 post_message "*******************************************************************"
-post_message "Generated programming file: LimeSDR-USB_lms7_trx.rbf" -submsgs [list "Ouput file saved in /output_files directory"]
+post_message "Generated programming file: LimeSDR-USB_lms7_trx_HW_1.4" -submsgs [list "Output file saved in /output_files directory"]
 post_message "*******************************************************************"
