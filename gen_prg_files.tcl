ae80eb554ca68e1680964f65c9f638c33539d73d
vytautasb <vytautas.buitvydas@gmail.com>
2017-02-14 14:12:40 +0200

    Test pattern for tx path

--- gen_prg_files.tcl
+++ gen_prg_files.tcl
@@ -6,5 +6,5 @@ post_message "Generated programming file: LimeSDR-USB_lms7_trx_HW_1.4.jic" -subm
 post_message "*******************************************************************"
 qexec "quartus_cpf -c output_files/rbf_file_setup.cof"
 post_message "*******************************************************************"
-post_message "Generated programming file: LimeSDR-USB_lms7_trx_HW_1.4" -submsgs [list "Output file saved in /output_files directory"]
+post_message "Generated programming file: LimeSDR-USB_lms7_trx_HW_1.4.rbf" -submsgs [list "Output file saved in /output_files directory"]
 post_message "*******************************************************************"
