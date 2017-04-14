2bb26fab28aa7e6049e93c377f5fac543db240d9
vytautasb <vytautas.buitvydas@gmail.com>
2017-04-14 16:06:57 +0300

    test cnt with negative values

--- src/testing/test_data_dd.vhd
+++ src/testing/test_data_dd.vhd
@@ -33,9 +33,10 @@ end test_data_dd;
 -- ----------------------------------------------------------------------------
 architecture arch of test_data_dd is
 --declare signals,  components here
-signal cnt_h : unsigned (9 downto 0);
-signal cnt_l : unsigned (9 downto 0);
-signal iq_sel	: std_logic;
+signal cnt_h 		: unsigned (11 downto 0);
+signal cnt_l 		: unsigned (11 downto 0);
+
+signal iq_sel		: std_logic;
 signal iq_sel_out	: std_logic; 
 
   
@@ -44,20 +45,33 @@ begin
   process(reset_n, clk)
     begin
       if reset_n='0' then
-		  cnt_h<=(others=>'0');
-        cnt_l<=(others=>'0');
 		  iq_sel<='0'; 
  	    elsif (clk'event and clk = '1') then
 			iq_sel<= not iq_sel;
-			cnt_l<=cnt_l+1;
- 	      cnt_h<=cnt_h+1;
  	    end if;
     end process;
 	 
+process(reset_n, clk)
+	begin
+		if reset_n='0' then
+			cnt_h <="000000000001"; 
+			cnt_l <=(others=>'0');
+		elsif (clk'event and clk = '1') then
+			if iq_sel = '0' then 
+				cnt_h <= cnt_h + 2;
+				cnt_l <= cnt_l + 2;
+			else 
+				cnt_h <= cnt_h;
+				cnt_l <= cnt_l;
+			end if;
+		end if;
+end process;
+	 
+	 
 iq_sel_out<= iq_sel when fr_start='0' else not iq_sel;
 
-data_l<=iq_sel_out & "0" & std_logic_vector(cnt_l) & '0' when mimo_en='1' else iq_sel_out & "00" & std_logic_vector(cnt_l);
-data_h<=iq_sel_out & "0" & std_logic_vector(cnt_h) & '1' when mimo_en='1' else iq_sel_out & "00" & std_logic_vector(cnt_h);
+data_l<=iq_sel_out & std_logic_vector(cnt_l);
+data_h<=iq_sel_out & std_logic_vector(cnt_h);
   
 end arch;
 
