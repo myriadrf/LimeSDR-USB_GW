7e6d74589c4f1d749758a34c7d56716d847b0443
vytautasb <v.buitvydas@limemicro.com>
2019-01-23 09:54:32 +0200

    Modified pct_separate files to work with 16b samples

--- src/tx_path_top/pct_separate/synth/one_pct_fifo.vhd
+++ src/tx_path_top/pct_separate/synth/one_pct_fifo.vhd
@@ -9,6 +9,7 @@
 -- ----------------------------------------------------------------------------
 --NOTES:
 -- ----------------------------------------------------------------------------
+-- altera vhdl_input_version vhdl_2008
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -21,7 +22,8 @@ entity one_pct_fifo is
    generic(
       dev_family              : string := "Cyclone IV E";
       g_INFIFO_DATA_WIDTH     : integer := 32;
-      g_PCTFIFO_SIZE          : integer := 4096; -- Packet FIFO size in bytes
+      g_MAX_PCT_SIZE          : integer := 4096; -- Maximum packet size in bytes 
+      g_PCT_HDR_SIZE          : integer := 16;   -- Packet header size in bytes
       g_PCTFIFO_RDATA_WIDTH   : integer := 128
    );
    port (
@@ -33,7 +35,7 @@ entity one_pct_fifo is
       pct_rdclk         : in  std_logic;
       pct_aclr_n        : in  std_logic;
       pct_rdy           : out std_logic;
-      pct_header        : out std_logic_vector(127 downto 0);
+      pct_header        : out std_logic_vector(g_PCT_HDR_SIZE*8-1 downto 0);
       pct_data_rdreq    : in  std_logic;
       pct_data          : out std_logic_vector(g_PCTFIFO_RDATA_WIDTH-1 downto 0);
       pct_data_rdempty  : out std_logic
@@ -46,40 +48,51 @@ end one_pct_fifo;
 -- ----------------------------------------------------------------------------
 architecture arch of one_pct_fifo is
 --declare signals,  components here
+-- Constants
+constant c_MAX_PCT_WORDS         : integer := g_MAX_PCT_SIZE*8/g_PCTFIFO_RDATA_WIDTH;
+constant c_PCT_HDR_WORDS         : integer := g_PCT_HDR_SIZE*8/g_PCTFIFO_RDATA_WIDTH;
+constant c_RD_RATIO              : integer := g_PCTFIFO_RDATA_WIDTH/8;
+
+-- inst0
 signal inst0_pct_wrreq           : std_logic;          
 signal inst0_pct_data            : std_logic_vector(g_INFIFO_DATA_WIDTH-1 downto 0);
-signal inst0_pct_header          : std_logic_vector(127 downto 0);
+signal inst0_pct_header          : std_logic_vector(g_PCT_HDR_SIZE*8-1 downto 0);
 signal inst0_pct_header_valid    : std_logic;
 
---inst1
-constant c_INST1_WRUSEDW_WIDTH   : integer := FIFO_WORDS_TO_Nbits(g_PCTFIFO_SIZE/(g_INFIFO_DATA_WIDTH/8),true);
-constant c_INST1_RDUSEDW_WIDTH   : integer := FIFO_WORDS_TO_Nbits(g_PCTFIFO_SIZE/(g_PCTFIFO_RDATA_WIDTH/8),true);
+-- inst1
+constant c_INST1_WRUSEDW_WIDTH   : integer := FIFO_WORDS_TO_Nbits(g_MAX_PCT_SIZE/(g_INFIFO_DATA_WIDTH/8),true);
+constant c_INST1_RDUSEDW_WIDTH   : integer := FIFO_WORDS_TO_Nbits(g_MAX_PCT_SIZE/(g_PCTFIFO_RDATA_WIDTH/8),true);
 signal inst1_reset_n             : std_logic;
 signal inst1_wrempty             : std_logic;
 signal inst1_rdusedw             : std_logic_vector(c_INST1_RDUSEDW_WIDTH-1 downto 0);
 signal inst1_rdempty             : std_logic;
 
---inst2
+-- inst2
 signal inst2_rdreq               : std_logic;
-signal inst2_q                   : std_logic_vector(127 downto 0);
+signal inst2_q                   : std_logic_vector(g_PCT_HDR_SIZE*8-1 downto 0);
 signal inst2_rdempty             : std_logic;
 
-signal current_pct_size          : std_logic_vector(15 downto 0);
-signal current_pct_size_valid    : std_logic;
-
+-- internal signals
+signal pct_header_valid          : std_logic;
+signal pct_words                 : unsigned(15 downto 0);
 signal pct_rdy_reg               : std_logic;
 
   
 begin
 -- ----------------------------------------------------------------------------
-    -- Reset logic
+-- Reset logic
 -- ----------------------------------------------------------------------------  
    sync_reg0 : entity work.sync_reg 
-   port map(clk, pct_aclr_n, '1', inst1_reset_n);
-   
+   port map(clk, pct_aclr_n AND reset_n, '1', inst1_reset_n);
+
+-- ----------------------------------------------------------------------------
+-- Packet separate state machine
+-- ----------------------------------------------------------------------------    
    inst0_pct_separate_fsm : entity work.pct_separate_fsm
    generic map(
-      g_DATA_WIDTH   => g_INFIFO_DATA_WIDTH
+      g_DATA_WIDTH   => g_INFIFO_DATA_WIDTH,
+      g_MAX_PCT_SIZE => g_MAX_PCT_SIZE,
+      g_PCT_HDR_SIZE => g_PCT_HDR_SIZE
    )
    port map(
       clk               => clk,
@@ -94,7 +107,9 @@ begin
       pct_header_valid  => inst0_pct_header_valid
    );
    
-   
+-- ----------------------------------------------------------------------------
+-- FIFO for storing one packet
+-- ----------------------------------------------------------------------------   
    inst1_fifo_inst : entity work.fifo_inst   
    generic map(
       dev_family     => dev_family,
@@ -118,13 +133,16 @@ begin
       rdempty     => inst1_rdempty,
       rdusedw     => inst1_rdusedw           
    );
-   
+
+-- ----------------------------------------------------------------------------
+-- FIFO for storing packet header
+-- ----------------------------------------------------------------------------
    inst2_fifo_inst : entity work.fifo_inst   
    generic map(
       dev_family     => dev_family,
-      wrwidth        => 128,
+      wrwidth        => g_PCT_HDR_SIZE*8,
       wrusedw_witdth => 3, 
-      rdwidth        => 128,
+      rdwidth        => g_PCT_HDR_SIZE*8,
       rdusedw_width  => 3,   
       show_ahead     => "OFF"
    )
@@ -144,25 +162,54 @@ begin
    );
    
    inst2_rdreq <= NOT inst2_rdempty;
-   
-   proc_name : process(pct_rdclk, inst1_reset_n)
+
+-- ----------------------------------------------------------------------------
+-- Internal processes 
+-- ----------------------------------------------------------------------------
+   -- Packet header valid
+   pct_hdr_valid_proc : process(pct_rdclk, reset_n)
    begin
-      if inst1_reset_n = '0' then 
-         current_pct_size        <= (others=>'1');
-         current_pct_size_valid  <= '0';
-         pct_rdy_reg             <= '0';
+      if reset_n = '0' then 
+         pct_header_valid <= '0';
+      elsif (pct_rdclk'event AND pct_rdclk='1') then 
+         pct_header_valid <= inst2_rdreq;
+      end if;
+   end process;
+   
+   -- Capture packet size in bytes from packet header and convert to FIFO read words count
+   pct_words_proc : process(pct_rdclk, reset_n)
+   begin
+      if reset_n = '0' then 
+         pct_words  <= (others=>'1');
       elsif (pct_rdclk'event AND pct_rdclk='1') then
-         current_pct_size_valid <= inst2_rdreq;
          
          if pct_data_rdreq = '1' OR inst1_rdempty = '1' then 
-            current_pct_size <= (others=>'1');
-         elsif current_pct_size_valid = '1' then 
-            current_pct_size <= "00" & inst2_q(23 downto 10);
+            pct_words <= (others=>'1');
+         elsif pct_header_valid = '1' then
+            -- For compatibility: if there are no packet size inserted in packet header
+            --                    then  pct_words = max number of packet words
+            if inst2_q(23 downto 8) = "0000000000000000" then 
+               pct_words <= to_unsigned(c_MAX_PCT_WORDS,pct_words'length);
+            else 
+               pct_words <= unsigned(inst2_q(23 downto 8))/c_RD_RATIO + c_PCT_HDR_WORDS;
+            end if;
          else 
-            current_pct_size <= current_pct_size;
+            pct_words <= pct_words;
          end if;
          
-         if unsigned(current_pct_size) = unsigned(inst1_rdusedw) then 
+      end if;
+   end process;
+   
+-- ----------------------------------------------------------------------------
+-- Output registers
+-- ---------------------------------------------------------------------------- 
+   out_reg : process(pct_rdclk, reset_n)
+   begin
+      if reset_n = '0' then 
+         pct_rdy_reg <= '0';
+      elsif (pct_rdclk'event AND pct_rdclk='1') then 
+      
+         if unsigned(inst1_rdusedw) = pct_words  then 
             pct_rdy_reg <= '1';
          else
             pct_rdy_reg <= '0';
@@ -171,6 +218,9 @@ begin
       end if;
    end process;
    
+-- ----------------------------------------------------------------------------
+-- Output ports
+-- ----------------------------------------------------------------------------   
    pct_rdy           <= pct_rdy_reg;
    pct_header        <= inst2_q;
    pct_data_rdempty  <= inst1_rdempty;
--- src/tx_path_top/pct_separate/synth/pct_separate_fsm.vhd
+++ src/tx_path_top/pct_separate/synth/pct_separate_fsm.vhd
@@ -20,7 +20,8 @@ use ieee.numeric_std.all;
 entity pct_separate_fsm is
    generic(
       g_DATA_WIDTH   : integer := 32;
-      g_HDR_SIZE     : integer := 16   -- Packet header size in bytes
+      g_MAX_PCT_SIZE : integer := 4096;   -- Maximum packet size in bytes 
+      g_PCT_HDR_SIZE : integer := 16      -- Packet header size in bytes
    );
    port (
       clk               : in  std_logic;
@@ -31,7 +32,7 @@ entity pct_separate_fsm is
       pct_wrreq         : out std_logic;
       pct_data          : out std_logic_vector(g_DATA_WIDTH-1 downto 0);
       pct_wrempty       : in  std_logic;
-      pct_header        : out std_logic_vector(127 downto 0);
+      pct_header        : out std_logic_vector(g_PCT_HDR_SIZE*8-1 downto 0);
       pct_header_valid  : out std_logic
 
    );
@@ -42,20 +43,24 @@ end pct_separate_fsm;
 -- ----------------------------------------------------------------------------
 architecture arch of pct_separate_fsm is
 --declare signals,  components here
-constant c_HDR_WORDS_N  : integer := g_HDR_SIZE*8/g_DATA_WIDTH; 
+   -- Constants
+constant c_MAX_PCT_WORDS   : integer := g_MAX_PCT_SIZE*8/g_DATA_WIDTH;
+constant c_PCT_HDR_WORDS   : integer := g_PCT_HDR_SIZE*8/g_DATA_WIDTH;
+constant c_RD_RATIO        : integer := g_DATA_WIDTH/8;
+
+-- Internal signals
 type state_type is (idle, rd_pct, rd_done);
 signal current_state, next_state : state_type;
+
 signal infifo_rdreq_int    : std_logic;
 signal infifo_data_valid   : std_logic;
 signal pct_wrreq_reg       : std_logic;
 signal rd_cnt              : unsigned(15 downto 0);
 signal wr_cnt              : unsigned(15 downto 0);
 signal rd_cnt_max          : unsigned(15 downto 0);
-signal header_0            : std_logic_vector(31 downto 0);
-signal header_1            : std_logic_vector(31 downto 0);
-signal header_2            : std_logic_vector(31 downto 0);
-signal header_3            : std_logic_vector(31 downto 0);
-signal header_valid        : std_logic;
+signal header_0            : std_logic_vector(63 downto 0);
+signal header_1            : std_logic_vector(63 downto 0);
+signal header_valid_reg    : std_logic;
 
   
 begin
@@ -75,7 +80,8 @@ end process;
 -- ----------------------------------------------------------------------------
 -- state machine combo
 -- ----------------------------------------------------------------------------
-fsm : process(current_state, infifo_rdempty, pct_wrempty, rd_cnt, wr_cnt, rd_cnt_max) begin
+fsm : process( current_state, infifo_rdempty, pct_wrempty, rd_cnt, wr_cnt, 
+               rd_cnt_max) begin
    next_state <= current_state;
    case current_state is
    
@@ -89,6 +95,8 @@ fsm : process(current_state, infifo_rdempty, pct_wrempty, rd_cnt, wr_cnt, rd_cnt
       when rd_pct =>
          if rd_cnt < rd_cnt_max - 1 then 
             next_state <= rd_pct;
+         elsif rd_cnt = rd_cnt_max - 1 AND infifo_rdempty = '1' then 
+            next_state <= rd_pct;
          else 
             next_state <= rd_done;
          end if;
@@ -99,13 +107,14 @@ fsm : process(current_state, infifo_rdempty, pct_wrempty, rd_cnt, wr_cnt, rd_cnt
          else
             next_state <= rd_done;
          end if;
-         
-         
+                  
       when others => 
          next_state <= idle;
+         
    end case;
 end process;
 
+-- FIFO read request
 process (current_state, infifo_rdempty) 
    begin 
       if current_state = rd_pct AND infifo_rdempty = '0' then 
@@ -114,7 +123,8 @@ process (current_state, infifo_rdempty)
          infifo_rdreq_int <= '0';
       end if;
    end process;
-   
+
+-- FIFO data valid   
 data_valid_proc : process(clk, reset_n)
 begin
    if reset_n = '0' then 
@@ -123,7 +133,8 @@ begin
       infifo_data_valid <= infifo_rdreq_int;
    end if;
 end process;
-   
+
+-- Read counter to know when to terminate read process
 rd_cnt_proc : process(clk, reset_n)
 begin
    if reset_n = '0' then 
@@ -138,7 +149,8 @@ begin
       end if;
    end if;
 end process;
-   
+ 
+-- Write counter to know when to capture packet header and data 
 wr_cnt_proc : process(clk, reset_n)
 begin
    if reset_n = '0' then 
@@ -154,70 +166,59 @@ begin
    end if;
 end process;
 
-hdr_cap : process(clk, reset_n)
+-- Packet header registers
+hdr_cap : process(clk)
 begin
    if (clk'event AND clk='1') then 
    
       if wr_cnt = 0 AND infifo_data_valid= '1' then 
-         header_0 <= infifo_data;
+         header_0 <= infifo_data(63 downto 0);
+         header_1 <= infifo_data(127 downto 64);
       end if;
       
-      if wr_cnt = 1 AND infifo_data_valid= '1' then 
-         header_1 <= infifo_data;
-      end if;
-      
-      if wr_cnt = 2 AND infifo_data_valid= '1' then 
-         header_2 <= infifo_data;
-      end if;
-      
-      if wr_cnt = 3 AND infifo_data_valid= '1' then 
-         header_3 <= infifo_data;
-      end if;
-
    end if;
 end process;
 
-headr_cap : process(clk, reset_n)
+-- Capture max read counter value from packet header
+rdcnt_max_proc : process(clk, reset_n)
 begin
    if reset_n = '0' then 
-      rd_cnt_max     <= (others=>'0');
-      header_valid   <= '0';
+      rd_cnt_max     <= (others=>'0');     
    elsif (clk'event AND clk='1') then
-      
-      if wr_cnt = 3 AND infifo_data_valid= '1' then 
-         header_valid   <= '1';
-      else 
-         header_valid   <= '0';
-      end if;
-      
-      
-      
+   
       if current_state = idle then 
          rd_cnt_max <= (others=>'1');
       elsif wr_cnt = 0 AND infifo_data_valid= '1' then
-         rd_cnt_max           <= unsigned("00" & infifo_data(23 downto 10)) + c_HDR_WORDS_N;
+         if infifo_data(23 downto 8) = "0000000000000000" then 
+            rd_cnt_max  <= to_unsigned(c_MAX_PCT_WORDS,rd_cnt_max'length);
+         else 
+            rd_cnt_max  <= unsigned(infifo_data(23 downto 8))/c_RD_RATIO + c_PCT_HDR_WORDS;
+         end if;
       else 
-         rd_cnt_max           <= rd_cnt_max;
+         rd_cnt_max  <= rd_cnt_max;
       end if;
       
    end if;
 end process;
 
-
-   
 -- ----------------------------------------------------------------------------
 -- Output registers
 -- ----------------------------------------------------------------------------
 out_reg : process(clk, reset_n)
 begin
    if reset_n = '0' then 
-      pct_wrreq_reg  <= '0';
+      pct_wrreq_reg     <= '0';
+      header_valid_reg  <= '0';
    elsif (clk'event AND clk='1') then
-      if rd_cnt < c_HDR_WORDS_N then 
-         pct_wrreq_reg <= '0';
-      else
-         pct_wrreq_reg  <= infifo_rdreq_int;
+
+      pct_wrreq_reg  <= infifo_rdreq_int;
+      
+      if wr_cnt = 0 AND infifo_data_valid= '1' then 
+         header_valid_reg   <= '1';
+      else 
+         header_valid_reg   <= '0';
       end if;
+      
    end if;
 end process;   
    
@@ -227,8 +228,8 @@ end process;
 infifo_rdreq      <= infifo_rdreq_int; 
 pct_wrreq         <= pct_wrreq_reg;
 pct_data          <= infifo_data;
-pct_header        <= header_3 & header_2 & header_1 & header_0;
-pct_header_valid  <= header_valid;
+pct_header        <= header_1 & header_0;
+pct_header_valid  <= header_valid_reg;
 
 
 end arch;   
