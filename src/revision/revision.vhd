6a35d8993b50759644e379c4e0940373d0dc956e
vytautasb <v.buitvydas@limemicro.com>
2019-05-07 10:44:32 +0300

    Ver.2.21

--- src/revision/revision.vhd
+++ src/revision/revision.vhd
@@ -14,11 +14,11 @@ package revisions is
 	constant MAJOR_REV : INTEGER := 2;
 	constant MINOR_REV : INTEGER := 1;
 	constant BETA_REV : INTEGER := 1;
-	constant COMPILE_REV : INTEGER := 20;
+	constant COMPILE_REV : INTEGER := 21;
 	constant COMPILE_YEAR_STAMP : INTEGER := 19;
-	constant COMPILE_MONTH_STAMP : INTEGER := 01;
-	constant COMPILE_DAY_STAMP : INTEGER := 29;
-	constant COMPILE_HOUR_STAMP : INTEGER := 15;
+	constant COMPILE_MONTH_STAMP : INTEGER := 05;
+	constant COMPILE_DAY_STAMP : INTEGER := 03;
+	constant COMPILE_HOUR_STAMP : INTEGER := 14;
 	
 	constant MAGIC_NUM : STD_LOGIC_VECTOR(31 downto 0) := X"D8A5F009";
    constant BOARD_ID : STD_LOGIC_VECTOR(15 downto 0) := X"000E"; -- LimeSDR-USB
